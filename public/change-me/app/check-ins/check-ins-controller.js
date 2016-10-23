249e825ee9d0d89d8fe39f872eedacaaded45b30
achernyak <artem@ikayzo.com>
2016-10-22 21:48:43 -0500

    check in and icons

--- public/change-me/app/check-ins/check-ins-controller.js
+++ public/change-me/app/check-ins/check-ins-controller.js
@@ -12,8 +12,37 @@
     .module('checkIns')
     .controller('CheckInsCtrl', CheckInsCtrl);
 
-  function CheckInsCtrl() {
+  function CheckInsCtrl($stateParams, $log, $http) {
     var vm = this;
+    var id = $stateParams.id;
+    var shelter = null;
+
+    $http.get('http://localhost:3000/shelters/' + id)
+      .then(setShelter);
+
+    vm.inc = function () {
+      $http.patch('http://localhost:3000/shelters/' + id + '/checkin')
+	.then(setShelter);
+    };
+
+    vm.dec = function () {
+      $http.patch('http://localhost:3000/shelters/' + id + '/checkin')
+	.then(setShelter);
+    };
+
+    vm.counts = function () {
+      if (shelter === null) {
+        return 'Loading';
+      }
+
+      return shelter.checkins + ' / ' + shelter.capacity;
+    };
+
     vm.ctrlName = 'CheckInsCtrl';
+
+    function setShelter(response) {
+      shelter = response.data;
+      $log.info(shelter);
+    }
   }
 }());
